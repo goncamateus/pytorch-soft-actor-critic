c14d5fcfc32e02cd87f7b9316b23ad2f21bc1899
goncamateus <mgm4@cin.ufpe.br>
2020-07-27 20:14:04 -0300

    with goncas modifications

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import argparse
 import datetime
+import itertools
+
 import gym
 import numpy as np
-import itertools
 import torch
-from sac import SAC
 from torch.utils.tensorboard import SummaryWriter
-from replay_memory import ReplayMemory
+
+import gym_line_follower
+from gym_line_follower.envs import LineFollowerEnv
+from replay_memory import ReplayGMemory, ReplayMemory
+from sac import SAC
 
 parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
-parser.add_argument('--env-name', default="HalfCheetah-v2",
-                    help='Mujoco Gym environment (default: HalfCheetah-v2)')
+parser.add_argument('--env-name', default="LineFollower-v0",
+                    help='Mujoco Gym environment (default: LineFollower-v0)')
 parser.add_argument('--policy', default="Gaussian",
                     help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
 parser.add_argument('--eval', type=bool, default=True,
@@ -48,22 +52,25 @@ args = parser.parse_args()
 
 # Environment
 # env = NormalizedActions(gym.make(args.env_name))
-env = gym.make(args.env_name)
+# env = gym.make(args.env_name)
+env = LineFollowerEnv(gui=False, sub_steps=1, max_track_err=0.05,
+                      max_time=100, power_limit=0.99)
+
 env.seed(args.seed)
-env.action_space.seed(args.seed)
+# env.action_space.seed(args.seed)
 
 torch.manual_seed(args.seed)
 np.random.seed(args.seed)
 
 # Agent
-agent = SAC(env.observation_space.shape[0], env.action_space, args)
+agent = SAC(env.observation_space.shape[0]+5, env.action_space, args)
 
-#Tesnorboard
+# Tesnorboard
 writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.env_name,
-                                                             args.policy, "autotune" if args.automatic_entropy_tuning else ""))
+                                                     args.policy, "autotune" if args.automatic_entropy_tuning else ""))
 
 # Memory
-memory = ReplayMemory(args.replay_size, args.seed)
+memory = ReplayGMemory(args.replay_size, args.seed)
 
 # Training Loop
 total_numsteps = 0
@@ -73,19 +80,31 @@ for i_episode in itertools.count(1):
     episode_reward = 0
     episode_steps = 0
     done = False
+    episode = []
     state = env.reset()
-
+    objectives = np.array(list(zip(env.track.x, env.track.y))[1:])
+    robot_pos = env._get_info()
+    robot_pos = np.array(list(robot_pos.values()))
+    state = np.concatenate([state, robot_pos])
+    worst_goal = np.array([[np.min(env.track.x), np.min(env.track.y)],
+                           [np.max(env.track.x), np.max(env.track.y)]])
+    worst_dist = np.linalg.norm(worst_goal[0]-worst_goal[1])
     while not done:
+        percentage = env.position_on_track/env.track.length
+        percentage = percentage if percentage > 0 else 0
+        objective_idx = int(objectives.shape[0]*percentage)
+        objective = objectives[objective_idx]
         if args.start_steps > total_numsteps:
             action = env.action_space.sample()  # Sample random action
         else:
-            action = agent.select_action(state)  # Sample action from policy
+            action = agent.select_action(np.concatenate([state, objective]))
 
         if len(memory) > args.batch_size:
             # Number of updates per step in environment
             for i in range(args.updates_per_step):
                 # Update parameters of all the networks
-                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
+                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(
+                    memory, args.batch_size, updates)
 
                 writer.add_scalar('loss/critic_1', critic_1_loss, updates)
                 writer.add_scalar('loss/critic_2', critic_2_loss, updates)
@@ -94,49 +113,70 @@ for i_episode in itertools.count(1):
                 writer.add_scalar('entropy_temprature/alpha', alpha, updates)
                 updates += 1
 
-        next_state, reward, done, _ = env.step(action) # Step
+        next_state, reward, done, robot_pos = env.step(action)  # Step
+        if len(episode) % 49 != 0:
+            reward = 0
+        robot_pos = np.array(list(robot_pos.values()))
+        next_state = np.concatenate([next_state, robot_pos])
         episode_steps += 1
         total_numsteps += 1
         episode_reward += reward
 
         # Ignore the "done" signal if it comes from hitting the time horizon.
         # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
-        mask = 1 if episode_steps == env._max_episode_steps else float(not done)
-
-        memory.push(state, action, reward, next_state, mask) # Append transition to memory
+        mask = float(not done)
+        # Append transition to memory
+        memory.push(state, action, reward, next_state, mask, objective)
+        episode.append((state, action, reward, mask, next_state, objective))
 
         state = next_state
+
+    for i, (state, action, reward, done, next_state, goal) in enumerate(episode):
+        g1 = state[-2:]
+        g2 = next_state[-2:]
+        d1 = np.linalg.norm(g1-goal)
+        d2 = np.linalg.norm(g2-goal)
+        if d2 > d1 and i > 0:
+            state_bef = episode[i-1][0]
+            action_bef = episode[i-1][1]
+            reward_bef = episode[i-1][2]
+            done_bef = episode[i-1][3]
+            new_reward = 1 - d1/worst_dist
+            memory.push(
+                state_bef, action_bef, new_reward, state, done_bef, g1)
 
     if total_numsteps > args.num_steps:
         break
 
     writer.add_scalar('reward/train', episode_reward, i_episode)
-    print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps, episode_steps, round(episode_reward, 2)))
+    print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(
+        i_episode, total_numsteps, episode_steps, round(episode_reward, 2)))
 
-    if i_episode % 10 == 0 and args.eval is True:
+    if i_episode % 100 == 0 and args.eval is True:
         avg_reward = 0.
         episodes = 10
-        for _  in range(episodes):
+        for _ in range(episodes):
             state = env.reset()
             episode_reward = 0
             done = False
             while not done:
+                env.render(mode='human')
                 action = agent.select_action(state, evaluate=True)
 
                 next_state, reward, done, _ = env.step(action)
                 episode_reward += reward
 
-
                 state = next_state
             avg_reward += episode_reward
         avg_reward /= episodes
 
-
         writer.add_scalar('avg_reward/test', avg_reward, i_episode)
 
         print("----------------------------------------")
-        print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
+        print("Test Episodes: {}, Avg. Reward: {}".format(
+            episodes, round(avg_reward, 2)))
         print("----------------------------------------")
+        agent.save_model(env_name=args.env_name, suffix=datetime.datetime.now(
+        ).strftime("%Y-%m-%d_%H-%M-%S"))
 
 env.close()
-
